c0b0c2d6cc28bd576d4e31a6540b2af80706a53c
Pascal Wagnière <pascal.wagniere@bluewin.ch>
2026-04-26 17:55:41 +0200

    moved server structure

--- public/tools.php
+++ public/tools.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('sqlite:./tools.db');
+    $db = new PDO('sqlite:../private/data/tools.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
